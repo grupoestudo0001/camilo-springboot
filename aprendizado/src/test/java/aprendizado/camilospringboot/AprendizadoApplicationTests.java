12b883ca45f10d650afc85347157d31fdc244317
eduardocamilo1985 <eduardocamilo1985@gmail.com>
2022-09-28 13:46:43 -0300

    primeiro commit do projeto

---
+++ aprendizado/src/test/java/aprendizado/camilospringboot/AprendizadoApplicationTests.java
@@ -0,0 +1,13 @@
+package aprendizado.camilospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprendizadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
